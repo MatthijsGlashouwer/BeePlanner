b39433120044d9d183d93dc975d42b717de23e1b
Matthijs Glashouwer <matthijsglashouwer@hotmail.com>
2018-05-15 11:55:31 +0200

    AUTH & TO edit
    AUTH & TO edit

---
+++ BeePlanner/resources/views/table.blade.php
@@ -0,0 +1,60 @@
+
+
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Uren registratie</div>
+
+                <div class="panel-body">
+                    <table class="table table-hover table-bordered table-striped datatable" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Week:  </th>
+                                <th>Opdrachtnummer</th>
+                                <th>Projectnaam</th>
+								<th>Totaal uren</th>
+								<th>Resterende uren</th>
+								<th>maandag</th>
+								<th>dinsdag</th>
+								<th>woensdag</th>
+								<th>donderdag</th>
+								<th>vrijdag</th>
+								<th>Uren invullen</th>
+                            </tr>
+                        </thead>
+						
+						<tbody id="uren">
+						
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								
+								<td>
+									<a class="btn btn-primary" href="">Invullen</a>
+								</td>
+								
+							</tr>
+							
+							
+							
+						
+						</tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
--- BeePlanner/routes/web.php
+++ BeePlanner/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/table', function() {
+	return view('table');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
